62d042cc2d5de9cb67c1705c66670f186dc6bf23
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-20 03:20:56 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Card from "./components/Card/Card";
+
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <h1>React App</h1>
-      <Card />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
