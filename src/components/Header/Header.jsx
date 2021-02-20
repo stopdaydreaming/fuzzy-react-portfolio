b205112f19ce1990d5a780b5b18a74d759ac3b9d
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-20 03:38:35 -0500

    added about page placeholder content

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,30 +7,28 @@ const Header = () => {
         <div class="container">
           <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
+              <h4 class="text-white">stopdaydreaming</h4>
               <p class="text-muted">
-                Add some information about the album below, the author, or any
-                other background context. Make it a few sentences long so folks
-                can pick up some informative tidbits. Then, link them off to
-                some social networking sites or contact information.
+                I re-created my developer portfolio using ReactJS and Bootstrap.
+                Take a peak at some of my recent projects and learn a bit more about me.
               </p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
+              <h4 class="text-white">MENU</h4>
               <ul class="list-unstyled">
                 <li>
                   <a href="#" class="text-white">
-                    Follow on Twitter
+                    About ME
                   </a>
                 </li>
                 <li>
                   <a href="#" class="text-white">
-                    Like on Facebook
+                    Portfolio
                   </a>
                 </li>
                 <li>
                   <a href="#" class="text-white">
-                    Email me
+                    Contact ME
                   </a>
                 </li>
               </ul>
@@ -41,22 +39,6 @@ const Header = () => {
       <div class="navbar navbar-dark bg-dark shadow-sm">
         <div class="container">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              aria-hidden="true"
-              class="me-2"
-              viewBox="0 0 24 24"
-            >
-              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-              <circle cx="12" cy="13" r="4" />
-            </svg>
             <strong>stopdaydreaming</strong>
           </a>
           <button
