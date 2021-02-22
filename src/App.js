508c2562c643d19f95deb3f713c31656573fedd4
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-22 14:35:48 -0500

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
