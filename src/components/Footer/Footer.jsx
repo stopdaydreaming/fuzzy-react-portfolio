91a343d255f77bca59b534689119d77962afe2e7
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-21 21:36:38 -0500

    added styles to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class="footer mt-auto py-3 bg-light">
-      <div class="container">
-        <span class="text-muted">&copy;2021. stopdaydreaming.</span>
+    <footer className="footer mt-auto py-3">
+      <div className="container">
+        <span className="text-light">&copy;2021. stopdaydreaming.</span>
       </div>
     </footer>
   );
