4768dfb4479e5ff1a36351f53d9df9c1e97e6d66
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-22 00:29:06 -0500

    add project component to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Project from "../components/Project/Project";
 
 const Portfolio = () => {
-    return (
-        <div>
-            <h1>Portfolio Page</h1>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mt-5">
+          <h1 className="page-title">Projects</h1>
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+          <Project />
+          <Project />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
