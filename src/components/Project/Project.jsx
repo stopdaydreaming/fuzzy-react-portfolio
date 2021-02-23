a97774e8064cca2ea6d6f1c7c010f8bf6e460210
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-22 22:15:50 -0500

    added props for data

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
-const Project = () => {
+const Project = (props) => {
   return (
-    <div class="col-md-6 mb-5 mt-5">
-      <div class="card">
+    <div className="col-md-6 mb-5 mt-5">
+      <div className="card">
         <img
-          src="https://via.placeholder.com/150"
-          class="card-img-top"
+          src={props.thumbnail}
+          className="card-img-top"
           alt="..."
         />
-        <div class="card-body">
-          <h5 class="text-muted">Project title</h5>
+        <div className="card-body">
+          <h5 className="text-muted">{props.title}</h5>
 
           <a
-            href="https://github.com/stopdaydreaming/mini-octo-burgers"
-            class="text-dark"
+            href={props.source}
+            className="text-dark"
             target="_blank"
           >
-            <button class="btn btn-dark mb-2 mr-3">Source Code</button>
+            <button className="btn btn-dark mb-2 mr-3">Source Code</button>
           </a>
           <a
-            href="https://mini-octo-burgers.herokuapp.com/"
-            class="text-dark"
+            href={props.demo}
+            className="text-dark"
             target="_blank"
           >
-            <button class="btn btn-dark mb-2">Live Demo</button>
+            <button className="btn btn-dark mb-2">Live Demo</button>
           </a>
         </div>
       </div>
